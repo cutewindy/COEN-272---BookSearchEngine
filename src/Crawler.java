eaf2037e283154602653bb45374c6d6f15787315
xiaotdl <xiaotdl@gmail.com>
2016-05-30 01:46:04 -0700

    minor improvements

--- src/Crawler.java
+++ src/Crawler.java
@@ -9,14 +9,14 @@ public class Crawler {
 	public static int pageId = 1;
 
 	// amazon
-//	public static int bookId = 1;
-//	public static final String SITE = "amazon";
-//	public static final String SEED = "http://www.amazon.com/gp/search/ref=sr_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cp_n_availability%3A2245266011%2Cp_n_fresh_match%3A1-2%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1464594881&rnid=1000";
+	public static int bookId = 1;
+	public static final String SITE = "amazon";
+	public static final String SEED = "http://www.amazon.com/gp/search/ref=sr_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cp_n_availability%3A2245266011%2Cp_n_fresh_match%3A1-2%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1464594881&rnid=1000";
 
 	// barnesNoble
-	public static int bookId = 10000;
-	public static final String SITE = "barnesNoble";
-	public static final String SEED = "http://www.amazon.com/gp/search/ref=sr_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cp_n_availability%3A2245266011%2Cp_n_fresh_match%3A1-2%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1464594881&rnid=1000";
+//	public static int bookId = 10000;
+//	public static final String SITE = "barnesNoble";
+//	public static final String SEED = "http://www.amazon.com/gp/search/ref=sr_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cp_n_availability%3A2245266011%2Cp_n_fresh_match%3A1-2%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1464594881&rnid=1000";
 
 	// betterWorld
 //	public static int bookId = 20000;
@@ -37,7 +37,7 @@ public class Crawler {
 			try {
 				Request request = this.requestQueue.remove(0);
 
-				System.out.printf("Visiting %s page: %s", request.type, request.url);
+				System.out.printf("Visiting %s page: %s\n", request.type, request.url);
 				if (request.isDuplicateUrl()) {
 					System.out.println("Found duplicate url!");
 					continue;
@@ -47,8 +47,8 @@ public class Crawler {
 					continue;
 				}
 				Response response = request.visit();
-				this.pageId++;
 				System.out.println("PageId: " + this.pageId);
+				this.pageId++;
 
 				if (response.statusCode() != 200) {
 					System.out.println("Request not successful! status code: " + String.valueOf(response.statusCode()));
